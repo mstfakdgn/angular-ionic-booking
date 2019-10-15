72ffe8e1be46a371c239906b6cb74616a8c90709
mstfakdgn <mustafa/akiler@gmail.com>
2019-10-15 11:16:24 +0300

    auth and modal added

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NavController, ModalController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,17 +11,34 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
-
-  constructor(private router: Router, private navCtrl: NavController) { }
+  place: Place;
+  constructor(
+    // private router: Router,
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placeService: PlacesService,
+    private modalCtrl: ModalController,
+    ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+      this.place = this.placeService.getPlace(paramMap.get('placeId'));
+    });
   }
 
   onBookPlace() {
     // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    // this.navCtrl.navigateBack('/places/tabs/discover');
     // pops the last page from stack
     // this.navCtrl.pop();
+    this.modalCtrl.create({component: CreateBookingComponent}).then(modalEl => {
+      modalEl.present();
+    });
+
 
   }
 
--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OffersService } from '../../offers.service';
+import { NavController } from '@ionic/angular';
+import { Offer } from '../../offer.model';
 
 @Component({
   selector: 'app-edit-offer',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
+  offer: Offer ;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private offerService: OffersService, private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.offer = this.offerService.getOffer(paramMap.get('placeId'));
+    });
   }
 
 }
