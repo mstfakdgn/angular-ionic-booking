e30aefa1db64702ccaa1f6630c2ad43965ad5723
mstfakdgn <mustafa/akiler@gmail.com>
2019-10-15 17:55:06 +0300

    segment and segment buttons added

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { SegmentChangeEventDetail } from '@ionic/core';
 // import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -10,6 +11,7 @@ import { Place } from '../place.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(
     private placesService: PlacesService,
@@ -18,6 +20,10 @@ export class DiscoverPage implements OnInit {
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.placesService.places.slice(1);
+  }
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
   // Opens the menu programatically
   // onOpenMenu() {
