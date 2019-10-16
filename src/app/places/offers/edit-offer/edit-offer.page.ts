30f2498cac7f5c3b4dc2e3d23a8f0ff829d0307c
mstfakdgn <mustafa/akiler@gmail.com>
2019-10-16 17:11:27 +0300

    form inputs and validations

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { OffersService } from '../../offers.service';
 import { NavController } from '@ionic/angular';
 import { Offer } from '../../offer.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -10,7 +11,8 @@ import { Offer } from '../../offer.model';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
-  offer: Offer ;
+  offer: Offer;
+  form: FormGroup;
 
   constructor(private route: ActivatedRoute, private offerService: OffersService, private navCtrl: NavController) { }
 
@@ -21,7 +23,25 @@ export class EditOfferPage implements OnInit {
         return;
       }
       this.offer = this.offerService.getOffer(paramMap.get('placeId'));
+      this.form = new FormGroup({
+        title: new FormControl(this.offer.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.offer.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        }),
+      });
     });
+
+  }
+
+  onEditOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+    console.log(this.form);
   }
 
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -7,11 +7,11 @@ import { Place } from './place.model';
 export class PlacesService {
   private _places: Place[] = [
     // tslint:disable-next-line: max-line-length
-    new Place('p1', 'Manhattan Mansion', 'In the heart of new york', 'https://previews.123rf.com/images/prometeus/prometeus1710/prometeus171000688/88190647-close-up-portrait-of-a-zombie-man-covered-with-snow-halloween-horror-film-.jpg', 150.00),
+    new Place('p1', 'Manhattan Mansion', 'In the heart of new york', 'https://previews.123rf.com/images/prometeus/prometeus1710/prometeus171000688/88190647-close-up-portrait-of-a-zombie-man-covered-with-snow-halloween-horror-film-.jpg', 150.00, new Date('2019-01-01'), new Date('2019-12-31')),
     // tslint:disable-next-line: max-line-length
-    new Place('p2', 'Washington Dc', 'In the heart of washington', 'https://i0.wp.com/www.aramamotoru.com/wp-content/uploads/2017/10/blog-yazilarinda-gorseller-nasil-secilmeli.jpg?fit=1150%2C500&ssl=1', 140.00),
+    new Place('p2', 'Washington Dc', 'In the heart of washington', 'https://i0.wp.com/www.aramamotoru.com/wp-content/uploads/2017/10/blog-yazilarinda-gorseller-nasil-secilmeli.jpg?fit=1150%2C500&ssl=1', 140.00, new Date('2019-01-01'), new Date('2019-12-31')),
     // tslint:disable-next-line: max-line-length
-    new Place('p3', 'Alabama', 'In the heart of Teksas', 'https://www.intersinema.com/haber/resimler/201907/25664_8034.jpg', 145.00),
+    new Place('p3', 'Alabama', 'In the heart of Teksas', 'https://www.intersinema.com/haber/resimler/201907/25664_8034.jpg', 145.00, new Date('2019-01-01'), new Date('2019-12-31')),
   ];
 
   get places() {
